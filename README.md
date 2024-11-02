3fcb7c727d65f59e88ef9df7e5097294fbcfe9cf
kprosek <klara.prosek@gmail.com>
2024-11-02 15:35:04 +0100

    Naloga 2, done

---
+++ README.md
@@ -0,0 +1,9 @@
+# ip21_akademija_projekt1
+
+Application for accessing cryptocurrency price data, built step by step.
+At its current state, the App makes an API call, decodes json file and displays the price of BTC.
+
+
+
+
+
--- console.php
+++ console.php
@@ -1,2 +1,10 @@
 <?php
-echo 'Hello World!';
+$apiUrl = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
+
+$json = file_get_contents($apiUrl);
+
+$displayData = json_decode($json, true);
+
+echo $displayData['data']['base'] . ': ';
+echo $displayData['data']['amount'] . ' ';
+echo $displayData['data']['currency'];
