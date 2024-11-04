79230f0903ec70cf82ccdaa7b3aeea810daaa9d9
kprosek <klara.prosek@gmail.com>
2024-11-04 19:48:36 +0100

    Naloga 4, done

--- console.php
+++ console.php
@@ -1,14 +1,23 @@
 <?php
 $currency = $argv[1];
 $pair = $argv[2];
+$apiUrl = 'https://api.coinbase.com/v2/prices/' . $currency . '-' . $pair . '/spot';
 
+// Error handling
 if (($currency === null || $pair === null) || $currency === 'help') {
     echo ('Help text');
     die;
+} else if ((strlen($currency) !== 3 && strlen($currency) !== 4) || strlen($pair) !== 3) {
+    echo ('Error: Wrong currency token');
+    die;
 }
 
-$apiUrl = 'https://api.coinbase.com/v2/prices/' . $currency . '-' . $pair . '/spot';
+if (file_get_contents($apiUrl) === false) {
+    echo ('Error: Wrong currency pair');
+    die;
+};
 
+// Valid data
 $json = file_get_contents($apiUrl);
 
 $displayData = json_decode($json, true);
