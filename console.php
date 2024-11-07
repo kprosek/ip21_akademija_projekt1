9e23e624ade261e3908792074b176745400a3e38
kprosek <klara.prosek@gmail.com>
2024-11-07 20:36:06 +0100

    Naloga 5, fix

--- console.php
+++ console.php
@@ -2,8 +2,6 @@
 $cryptoGet = $argv[1] ?? null;
 $currencyGet = $argv[2] ?? null;
 $apiUrl = 'https://api.coinbase.com/v2/prices/' . $cryptoGet . '-' . $currencyGet . '/spot';
-$apiCurrency = 'https://api.coinbase.com/v2/currencies';
-$apiCryptoCurrency = 'https://api.coinbase.com/v2/currencies/crypto';
 
 function getApiData($api)
 {
@@ -11,29 +9,42 @@ function getApiData($api)
     $dataArray = json_decode($json, true);
     return $dataArray;
 }
+if ($cryptoGet === 'help') {
+    echo sprintf('Error message: Need to add arguments in the input - example: BTC USD');
+    die;
+}
 
 // GET Currencies
-$currencies = getApiData($apiCurrency);
-$currenciesList = [];
-foreach ($currencies['data'] as $currency) {
-    $currenciesList[] = $currency['id'];
+function currencyList()
+{
+    $api = 'https://api.coinbase.com/v2/currencies';
+    $currencies = getApiData($api);
+    $list = [];
+    foreach ($currencies['data'] as $currency) {
+        $list[] = $currency['id'];
+    }
+    return $list;
 }
 
+$currencies = currencyList();
+
 // GET Crypto Currencies
-$crypto = getApiData($apiCryptoCurrency);
-$cryptoList = [];
-foreach ($crypto['data'] as $cry) {
-    $cryptoList[] = $cry['code'];
+function cryptoList()
+{
+    $api = 'https://api.coinbase.com/v2/currencies/crypto';
+    $crypto = getApiData($api);
+    $list = [];
+    foreach ($crypto['data'] as $cry) {
+        $list[] = $cry['code'];
+    }
+    return $list;
 }
+
+$crypto = cryptoList();
 
 // Error handling
 if ($cryptoGet === null || $currencyGet === null) {
     echo sprintf('Error message: Missing arguments in the input');
-    die;
-}
-
-if ($cryptoGet === 'help') {
-    echo sprintf('Error message: Need to add arguments in the input - example: BTC USD');
     die;
 }
 
