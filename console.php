98238f0ef1a9ef5c1ca87f147b7e9fa5ec811213
kprosek <klara.prosek@gmail.com>
2024-11-13 08:07:32 +0100

    Phase 2 - fix

--- console.php
+++ console.php
@@ -6,41 +6,38 @@ $commandGet = $argv[1] ?? null;
 $cryptoGet = $argv[2] ?? null;
 $currencyGet = $argv[3] ?? null;
 
-function printHelpText($cryptoGet, $currencyGet)
+function verifyArg($cryptoGet, $currencyGet)
 {
     if ($cryptoGet === null || $currencyGet === null) {
-        echo ('Error message: Argument cannot be null');
+        printHelpText('Error message: Argument cannot be null');
         die;
     }
 
     if ((strlen($cryptoGet) < 3 || strlen($cryptoGet) > 10)) {
-        echo ('Error message: Wrong crypto token length');
+        printHelpText('Error message: Wrong crypto token length');
         die;
     }
 
     if (strlen($currencyGet) !== 3) {
-        echo ('Error message: Wrong currency token length');
+        printHelpText('Error message: Wrong currency token length');
         die;
     }
 }
 
-function printErrorText($cryptoGet, $currencyGet)
+function verifyData($cryptoGet, $currencyGet, $cryptoList, $currenciesList, $currencyPair)
 {
-    $currenciesList = getListCurrencies();
-    $cryptoList = getListCrypto();
     if ($currenciesList === false || $cryptoList === false) {
-        echo ('Error message: Unsupported token pair, empty or invalid .json file for currencies or crypto token list');
+        printHelpText('Error message: Unsupported token pair, empty or invalid .json file for currencies or crypto token list');
         die;
     };
 
     if (in_array($cryptoGet, $cryptoList) === false || in_array($currencyGet, $currenciesList) === false) {
-        echo ('Error message: Invalid crypto or currency token');
+        printHelpText('Error message: Invalid crypto or currency token');
         die;
     }
 
-    $currencyPair = getCurrencyPair($cryptoGet, $currencyGet);
     if ($currencyPair === false) {
-        echo ('Error message: Unsupported token pair, empty or invalid .json file for currencies pair');
+        printHelpText('Error message: Unsupported token pair, empty or invalid .json file for currencies pair');
         die;
     };
 }
@@ -51,12 +48,16 @@ switch ($commandGet) {
         break;
     case 'list':
         $cryptoList = getListCrypto();
-        printlist($cryptoList);
+        printList($cryptoList);
         break;
     case 'price':
-        printHelpText($cryptoGet, $currencyGet);
-        printErrorText($cryptoGet, $currencyGet);
+        verifyArg($cryptoGet, $currencyGet);
+
+        $currenciesList = getListCurrencies();
+        $cryptoList = getListCrypto();
         $currencyPair = getCurrencyPair($cryptoGet, $currencyGet);
+        verifyData($cryptoGet, $currencyGet, $cryptoList, $currenciesList, $currencyPair);
+
         printPricePair($currencyPair);
         break;
     default:
--- lib/views/consoleView.php
+++ lib/views/consoleView.php
@@ -14,3 +14,8 @@ function printPricePair($currencyPair)
 
     echo sprintf('%s: %.2f %s', $currencyPair['data']['base'], $currencyPair['data']['amount'], $currencyPair['data']['currency']);
 }
+
+function printHelpText($text)
+{
+    echo ('Error message: ' . $text);
+}
